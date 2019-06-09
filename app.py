fd9bb89b007f353b5740f146295cf8629037d664
namnt <namnt@tictag.vn>
2019-06-09 09:11:58 +0700

    deploy heroku

--- app.py
+++ app.py
@@ -4,48 +4,47 @@ from flask import Flask, request
 from flask_restplus import Api, Resource, fields
 
 flask_app = Flask(__name__)
+flask_app.debug = True
 app = Api(app = flask_app,
 		  version = "1.0",
 		  title = "RestAPIs UI",
-		  description = "Manage names of various users of the application")
+		  description = "Manage routing of the application")
 
-name_space = app.namespace('names', description='Manage names')
-
+profile = app.namespace('profile', description='Own profile')
 model = app.model('Name Model',
 				  {'name': fields.String(required = True,
     					  				 description="Name of the person",
     					  				 help="Name cannot be blank.")})
 
-list_of_names = {}
+HTTP_CODE = {
+	'200': 'OK',
+	'400': 'Bad Request and Invalid Argument',
+	'403': 'FORBIDDEN',
+	'500': 'Mapping Key Error',
+}
 
-@name_space.route("/<int:id>")
-class MainClass(Resource):
 
-	@app.doc(responses={ 200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error' },
-			 params={ 'id': 'Specify the Id associated with the person' })
-	def get(self, id):
+@profile.route("/owner")
+class ProfileClass(Resource):
+
+	@app.doc(responses={ 200: HTTP_CODE['200'], 403: HTTP_CODE['403'], 500: HTTP_CODE['500'] },
+			 params={'': 'Get owner\'s project information'})
+	def get(self):
 		try:
-			name = list_of_names[id]
 			return {
-				"status": "Person retrieved",
-				"name" : list_of_names[id]
+				"name": 'Nguyen Thanh Nam',
+				"status": "Single",
+				"contact": {
+					"skype": "ngnamuit994",
+					"linked": "https://www.linkedin.com/in/nam-nguyen-thanh-3bb2b4117/",
+					"resume": "https://www.topcv.vn/xem-cv/68dc63745ac65e8682f02dcd73582943",
+					"email": "ngnamuit@gmail.com"
+				},
+				"notes": '''Finding a professional workplace environment.
+						   Trying to improve myself every day.
+						'''
 			}
 		except KeyError as e:
 			name_space.abort(500, e.__doc__, status = "Could not retrieve information", statusCode = "500")
 		except Exception as e:
 			name_space.abort(400, e.__doc__, status = "Could not retrieve information", statusCode = "400")
-
-	@app.doc(responses={ 200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error' },
-			 params={ 'id': 'Specify the Id associated with the person' })
-	@app.expect(model)
-	def post(self, id):
-		try:
-			list_of_names[id] = request.json['name']
-			return {
-				"status": "New person added",
-				"name": list_of_names[id]
-			}
-		except KeyError as e:
-			name_space.abort(500, e.__doc__, status = "Could not save information", statusCode = "500")
-		except Exception as e:
-			name_space.abort(400, e.__doc__, status = "Could not save information", statusCode = "400")
